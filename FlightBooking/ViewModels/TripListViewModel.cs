c387d692352a77739ec0014096ce87e031d263e4
HanBrol0 <43140648+HanBrol0@users.noreply.github.com>
2023-10-03 12:49:24 -0400

    Added description comment to TripListViewModel.cs

---
+++ FlightBooking/ViewModels/TripListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using FlightBooking.Models;
+
+namespace FlightBooking.ViewModels
+{
+    /*
+     * ViewModel Class to contain a list of Trips for displaying in Views
+     */
+    public class TripListViewModel
+    {
+        public IEnumerable<Trip> TripList { get; set; }
+    }
+}
