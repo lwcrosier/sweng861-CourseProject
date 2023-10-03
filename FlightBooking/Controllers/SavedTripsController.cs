31f8cc465eac2ba6b3b88c736535bf74ab9896e2
HanBrol0 <43140648+HanBrol0@users.noreply.github.com>
2023-10-03 09:51:50 -0400

    Added Class and Method documentation comments

--- FlightBooking/Controllers/SavedTripsController.cs
+++ FlightBooking/Controllers/SavedTripsController.cs
@@ -4,15 +4,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FlightBooking.Controllers
 {
+    /*
+     * Controller class for the 'SaveTrips' page set
+     */
     public class SavedTripsController : Controller
     {
         private readonly ITripRepository _tripRepository;
 
+        /*
+         * SavedTripsController() constructor 
+         * @param tripRepository is a TripRepository interface for dependency injection to allow use of Trip objects in the database
+         */
         public SavedTripsController(ITripRepository tripRepository)
         {
             _tripRepository = tripRepository;
         }
 
+        /*
+         * Index() controller method for calls to /SavedTrips/Index.cshtml
+         * @param None
+         * @return Views/SavedTrips/Index.cshtml with a ViewModel object containing a list of all Trip Objects
+         */
         public IActionResult Index()
         {
             TripListViewModel tlvm = new TripListViewModel
@@ -23,6 +35,11 @@ namespace FlightBooking.Controllers
             return View(tlvm);
         }
 
+        /*
+         * DeleteTrip(int id) calls the localhost api to delete the Trip corresponding to id
+         * @param id of the trip to delete
+         * @return a call the Index() to return Views/SavedTrips/Index.cshtml
+         */
         public async Task<IActionResult> DeleteTrip(int id)
         {
 
