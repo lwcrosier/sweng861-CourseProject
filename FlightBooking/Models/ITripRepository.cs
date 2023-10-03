b6efdc405a84f6c9fbc2a36ca8748ae8031ccbe7
HanBrol0 <43140648+HanBrol0@users.noreply.github.com>
2023-10-03 10:58:59 -0400

    Added documentation comment

---
+++ FlightBooking/Models/ITripRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace FlightBooking.Models
+{
+    /*
+     * Interface to interact with the TripRepository class
+     */
+    public interface ITripRepository
+    {
+        public IEnumerable<Trip> GetAllTrips();
+    }
+}
+
