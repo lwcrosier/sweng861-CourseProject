12b7011067568fd2698c0d7f1f5d58f3deb26c10
HanBrol0 <43140648+HanBrol0@users.noreply.github.com>
2023-10-03 10:58:42 -0400

    Added namespace to class

---
+++ FlightBooking/Models/TripInformation.cs
@@ -0,0 +1,9 @@
+namespace FlightBooking.Models;
+
+public class TripInformation
+{
+    public string Source { get; set; }
+    public string Destination { get; set; }
+    public DateTime Date { get; set; }
+
+}
