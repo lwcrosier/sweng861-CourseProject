3525c3bb87c8b10f7532f7d00c89be50f80c2ff0
HanBrol0 <43140648+HanBrol0@users.noreply.github.com>
2023-10-03 10:53:51 -0400

    Added AppDbContext.cs

---
+++ FlightBooking/Models/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FlightBooking.Models
+{
+    /*
+     * Class to represent database access for EntityFramework.
+     * Dataset for Trip and SearchResults objects
+     */
+    public class AppDbContext : DbContext
+    {
+        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Trip> Trips { get; set; } = null!;
+        public DbSet<TripSearchResults> SearchResults { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+        /*
+        Manually create table for sqlite:
+        
+        CREATE TABLE "__EFMigrationsHistory" (
+        "MigrationId" text NOT NULL,
+        "ProductVersion" text NOT NULL,
+        CONSTRAINT "PK_HistoryRow" PRIMARY KEY ("MigrationId"));
+        */
+    }
+}
