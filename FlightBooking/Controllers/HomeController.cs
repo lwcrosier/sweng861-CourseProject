5432cbece900c0ba606d0c7dfb670d24965e305c
HanBrol0 <43140648+HanBrol0@users.noreply.github.com>
2023-10-03 09:44:24 -0400

    Added Home Controller

---
+++ FlightBooking/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FlightBooking.Models;
+using Newtonsoft.Json;
+
+namespace FlightBooking.Controllers;
+
+/*
+ * Default, Boilerplate class used as the default home page
+ */
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    /*
+     * Index returns Views/FlightBooking/Index.cshtml as this class is meant to be unused unless testing
+     */
+    public IActionResult Index()
+    {
+        //return View();
+        return RedirectToAction("Index", "FlightBooking", new { area = "" });
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+}
