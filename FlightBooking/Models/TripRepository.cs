8f3cd3f446080441b2c945346b3e16dc05acc12a
HanBrol0 <43140648+HanBrol0@users.noreply.github.com>
2023-10-03 11:22:21 -0400

    Added class and method documentation to TripRepository

---
+++ FlightBooking/Models/TripRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FlightBooking.Models
+{
+    /*
+     * Class for database business logic of the TripRepository
+     */
+    public class TripRepository : ITripRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        /*
+         * TripRepository() constructor
+         * @param appDbContext is used to modify the database 
+         */
+        public TripRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        /*
+         * GetAllTrips() returns all trip objects saved in the Database
+         * @return a List of all Trip objects
+         */
+        public IEnumerable<Trip> GetAllTrips()
+        {
+            return _appDbContext.Trips.ToList();
+        }
+
+    }
+}
