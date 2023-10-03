3af7d767c58edc162e16485eb4815c3e9a3c4299
HanBrol0 <43140648+HanBrol0@users.noreply.github.com>
2023-10-03 10:59:11 -0400

    Added documentation comment

---
+++ FlightBooking/Models/ITripSearchResultsRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace FlightBooking.Models
+{
+    /*
+     * Interface to interact with the TripSearchResultsRepositoryClass
+     */
+    public interface ITripSearchResultsRepository
+    {
+        public void CacheSearchResultsAsync (TripSearchResults searchResults);
+        public Task<bool> CachedResultsAreCurrent(TripInformation tripInformation);
+        public Task<TripSearchResults> GetSearchResultsAsync(TripInformation tripInformation);
+    }
+}
